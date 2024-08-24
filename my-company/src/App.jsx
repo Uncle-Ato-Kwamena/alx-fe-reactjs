d5aa973a8ac106ce3eb45abca4875c2a8eb90216
Samuel <samuelmensah99@outlook.com>
2024-08-24 19:46:48 +0000

    first commit for my-company website

---
+++ my-company/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <Router>
+      <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ backgroundColor: 'navy', color: 'white', padding: '10px', textAlign: 'center', marginTop: '20px' }}>
+        <p>&copy; 2024 My Company website. By Saamuel Ato Nensah.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
