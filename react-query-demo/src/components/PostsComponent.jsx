f97f19c2e93d85bf82be0bc6e8dbd06891827a17
Samuel <samuelmensah99@outlook.com>
2024-09-08 22:53:31 +0000

    cache timer added

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 
-
+// Function to fetch posts from the API
 const fetchPosts = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
   if (!response.ok) {
@@ -11,8 +11,17 @@ const fetchPosts = async () => {
 };
 
 const PostsComponent = () => {
- 
-  const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts);
+  
+  const { data, error, isLoading, isError, refetch, isFetching } = useQuery(
+    'posts',
+    fetchPosts,
+    {
+      cacheTime: 1000 * 60 * 5, 
+      staleTime: 1000 * 60 * 1, 
+      refetchOnWindowFocus: true, 
+      keepPreviousData: true, 
+    }
+  );
 
   if (isLoading) return <div>Loading...</div>; 
   if (isError) return <div>Error: {error.message}</div>; 
@@ -20,7 +29,7 @@ const PostsComponent = () => {
   return (
     <div>
       <h1>Posts</h1>
-      <button onClick={() => refetch()}>Refetch Data</button> {}
+      <button onClick={() => refetch()} disabled={isFetching}>Refetch Data</button> {/* Button to manually refetch data */}
       <ul>
         {data && data.length > 0 ? ( 
           data.map(post => (
