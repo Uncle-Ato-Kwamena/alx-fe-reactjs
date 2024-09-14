c558015fbcf17c59567da42a03329301e927605b
Samuel <samuelmensah99@outlook.com>
2024-09-14 17:11:45 +0000

    latest responsiveness update

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
     return (
       <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-10 md:my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
         <img
-          className="rounded-full w-24 h-24 sm:w-32 sm:h-36 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+          className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
           src="https://via.placeholder.com/150"
           alt="User"
         />
