43199eb4f4afa2aeb2b838b2691ab2d49f5e40c0
Samuel <samuelmensah99@outlook.com>
2024-09-29 23:28:37 +0000

    errors corrected

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data; // Return user data if found
+  } catch (error) {
+    return null; // Return null if there is an error
+  }
 };
